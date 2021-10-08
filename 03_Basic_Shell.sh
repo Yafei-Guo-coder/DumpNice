7330e63115259d897e77ff55062f8b85cfc0bde8
Yafei Guo <yafeiguo116@gmail.com>
2021-10-08 16:38:31 +0800

    Update codes

--- 03_Basic_Shell.sh
+++ 03_Basic_Shell.sh
@@ -1,42 +1,14 @@
-#-------------------------------------------------------------------shell--------------------------------------------------------------------------------------
-#Linux Shell中使用awk完成两个文件的关联Join
-#外关联
+#awk
 awk 'NR==FNR{a[$2]=$0;}NR!=FNR{print $0,a[$2]}' b.txt a.txt
 #内关联
 #方法1
 awk -F',' 'NR==FNR{a[$1]=$2;}NR!=FNR && a[$1] {print $0,a[$1]}' b.txt a.txt
 #方法2
 awk -F',' 'NR==FNR{a[$1]=$2;}NR!=FNR && $1 in a {print $0,a[$1]}' b.txt a.txt
-
 awk 'FNR==NR{a[$1];next}($1 in a){next} {print}' a b 
-
-
-#-------------------------------------------------------------------shell--------------------------------------------------------------------------------------
-#提取vcf的特定区域
-#yafei@203:/data2/yafei/Project3/make_tree
-for chr in {1,2,7,8,13,14,19,20,25,26,31,32,37,38}
-do
-bedtools intersect -a /data2/xuebo/Projects/Speciation/tree/withBarley_segregate/chr${chr}.withBarley.vcf.gz -b merge_A.bed -header > chr${chr}.withBarley.vcf &
-  done
-
-for chr in {3,4,9,10,15,16,21,22,27,28,33,34,39,40}
-do
-bedtools intersect -a /data2/xuebo/Projects/Speciation/tree/withBarley_segregate/chr${chr}.withBarley.vcf.gz -b merge_B.bed -header > chr${chr}.withBarley.vcf &
-  done
-
-for chr in {5,6,11,12,17,18,23,24,29,30,35,36,41,42}
-do
-bedtools intersect -a /data2/xuebo/Projects/Speciation/tree/withBarley_segregate/chr${chr}.withBarley.vcf.gz -b merge_D.bed -header > chr${chr}.withBarley.vcf &
-  done
-
-for i in `ls *vcf`
-do 
-bgzip -c ${i} > ${i}.gz &
-  done
-
 awk 'ORS=NR%2?" ":"\n"{print}' 
   
-#批量杀死程序
+#shell
 ps aux|grep Volca|tail -n 20 | awk '{print $2}' > id
 for i in `cat id`; do kill -9 $i; done
 
@@ -54,20 +26,42 @@ done
 
 samtools view test.bam | awk '{print $5"\t"$9}'| sed '1i mapping-qulity\tmate-length' > test
 awk '{print $5"\t"$9}' standard.bam| sed '1i mapping-qulity\tmate-length' > standard
-
 cat 687_f1_test.fq | paste - - - - | sort -k1,1 -t " "  > 687_f1_test.sorte
-
 grep -v -f 687_uniqid 687_f1_test.sorted | tr "\t" "\n" > 687_f1_test.sorted.fq
-
-nohup zcat CRR061687_r2.filtered.fq.gz |paste - - - - |sort -k1,1 -S 500G | tr '\t' '\n' |gzip > CRR061687_r2.filtered_sorted.fq.gz &
+zcat CRR061687_r2.filtered.fq.gz |paste - - - - |sort -k1,1 -S 500G | tr '\t' '\n' |gzip > CRR061687_r2.filtered_sorted.fq.gz
+seqkit sort -n CRR061687_r2.filtered.fq.gz | gzip -c > CRR061687_r2.filtered_sorted.fq.gz
   
-nohup seqkit sort -n CRR061687_r2.filtered.fq.gz | gzip -c > CRR061687_r2.filtered_sorted.fq.gz &
-  
+#bcftools
+bedtools intersect -a /data2/xuebo/Projects/Speciation/tree/withBarley_segregate/chr${chr}.withBarley.vcf.gz -b merge_D.bed -header > chr${chr}.withBarley.vcf
+bedtools intersect -a /data1/home/yafei/009_GWAS/gene/gene_v1.1_Lulab.gff3 -b test -wb | awk 'split($9, array, ";") {print $1"\t"$4"\t"$5"\t"array[1]"\t"$10"\t"$11"\t"$12"\t"$13"\t"$14}' 
 bcftools merge chr0${chr}.vcf.gz chr0${chr}.vcf.gz chr0${chr}.vcf.gz chr${chr}.vcf.gz -o chr${chr}.all.vcf
-
 bcftools filter 1000Genomes.vcf.gz --regions 9:4700000-4800000 > 4700000-4800000.vcf
 
-#E6:xuebo@204:/data2/xuebo/Projects/Speciation/E6/Landrace_locate_225
-#gff & bed 取交集
+#vcftools
+#getVcf.sh
+cat $1 |while read gene chr from to taxa
+do
+    #echo $chr $from $to
+    #if echo $2 |grep -q '.*.vcf.gz$';then
+    #    vcftools --gzvcf $2 --chr $chr --from-bp $from --to-bp $to  --recode --recode-INFO-all --out $gene.$chr.$from-$to 
+    #elif echo $2 |grep -q '.*.vcf$';then
+        vcftools --gzvcf /data2/yafei/003_Project3/Vmap1.1/E6/VCF/chr${chr}.E6all.vcf.gz --keep ${taxa}_ancestor.txt --maf 0.0001 --chr $chr --from-bp $from --to-bp $to  --recode --recode-INFO-all --out 5k.$gene.$chr.$from-$to
+    #fi
+done
+
+#bedtools
+#提取vcf的特定区域
+for chr in {1,2,7,8,13,14,19,20,25,26,31,32,37,38}
+do
+bedtools intersect -a /data2/xuebo/Projects/Speciation/tree/withBarley_segregate/chr${chr}.withBarley.vcf.gz -b merge_A.bed -header > chr${chr}.withBarley.vcf &
+  done
+
+for chr in {3,4,9,10,15,16,21,22,27,28,33,34,39,40}
+do
+bedtools intersect -a /data2/xuebo/Projects/Speciation/tree/withBarley_segregate/chr${chr}.withBarley.vcf.gz -b merge_B.bed -header > chr${chr}.withBarley.vcf &
+  done
+
+for chr in {5,6,11,12,17,18,23,24,29,30,35,36,41,42}
+do
 bedtools intersect -a /data2/xuebo/Projects/Speciation/tree/withBarley_segregate/chr${chr}.withBarley.vcf.gz -b merge_D.bed -header > chr${chr}.withBarley.vcf &
-bedtools intersect -a /data1/home/yafei/009_GWAS/gene/gene_v1.1_Lulab.gff3 -b test -wb | awk 'split($9, array, ";") {print $1"\t"$4"\t"$5"\t"array[1]"\t"$10"\t"$11"\t"$12"\t"$13"\t"$14}' 
+  done
--- Vmap1.1R/04_Xp-clr_Haplotype_Detail.sh
+++ Vmap1.1R/04_Xp-clr_Haplotype_Detail.sh
@@ -7,6 +7,24 @@
   #eg(开花，光敏): TaFT8-2_1(TraesCS2A02G485000), TaPHYC_1(TraesCS5A02G391300)
 
 #working directory
-204:yafei:/data2/yafei/003_Project3/Vmap1.1/E6/Xp-CLR_V2
+204:yafei:/data2/yafei/003_Project3/Vmap1.1/E6/Xp-CLR_V2/VIP_genes
 #提取祖先的对应位点vcf
+#VIP_gene.txt
+TraesCS1D02G029100
+TraesCS1D02G040400
+TraesCS5B02G059000
+TraesCS1A02G242800
+TraesCS1A02G255100
+TraesCS4A02G271000
+TraesCS4B02G043100
+TraesCS2A02G485000
+TraesCS5A02G391300
+#提取带祖先的样本
+grep -w -f VIP_gene.txt 87gene_5k.txt > VIPgene_5k.txt
+bash getVcf.sh VIPgene_5k.txt > VIPgene_5k.log
+##传到本地用Java Migration/Haplotype将vcf转换成单倍型txt格式，使用07_VCF_Haplotype_Visual.r进行可视化。
+#本地路径：/Users/guoyafei/Documents/01_个人项目/02_Migration/02_数据表格/01_Vmap1-1/01_Add_ZNdata/05_Environment/XP-CLR/Gene/VIP_gene
+
+
+
 

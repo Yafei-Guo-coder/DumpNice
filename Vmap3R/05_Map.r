e68f25f5fb216740c9f1eb1a5403dd1ec0e7dd3a
Yafei Guo <yafeiguo116@gmail.com>
2021-10-10 09:39:10 +0800

    Update 05_Map.r

--- Vmap3R/05_Map.r
+++ Vmap3R/05_Map.r
@@ -5,7 +5,8 @@ library(ggmap)
 #library(maps)
 #library(psych)
 #画样本分布图----
-taxa <- read.table("/Users/guoyafei/Documents/01_个人项目/04_VmapIII/12_Vmap3Test/05_VcfCheck/taxa_location.txt", header=T, stringsAsFactors = F)
+setwd("/Users/guoyafei/Documents/01_个人项目/02_Migration/03_基本统计/13_Plots/09_Map/")
+taxa <- read.table("/Users/guoyafei/Documents/01_个人项目/02_Migration/03_基本统计/13_Plots/09_Map/795Taxa.txt", header=T, sep="\t",stringsAsFactors = F)
 taxa_AB <- taxa[which(taxa$Ploidy=="AABB"),]
 taxa_ABD <- taxa[which(taxa$Ploidy=="AABBDD"),]
 taxa_D <- taxa[which(taxa$Ploidy=="DD"),]
@@ -13,18 +14,22 @@ taxa_D <- taxa[which(taxa$Ploidy=="DD"),]
 #绘图
 mp<-NULL
 mapworld<-borders("world",colour = "gray70",fill="white") 
-mp<-ggplot()+mapworld+ylim(-90,90)+theme_classic()
+mp<-ggplot()+mapworld+ylim(-60,90) +theme_classic()
 
+color <- brewer.pal(8, "Dark2")[c(1,2,3,4,6)]
 #AABBDD----
-mp2<-mp+geom_point(aes(x=taxa_ABD$Longitude, y=taxa_ABD$Latitude, color=taxa_ABD$TreeValidatedGroupbySubspecies),size=1.5)+scale_size(range=c(1,1))+ theme_classic()+
+mp2<-mp+geom_point(aes(x=taxa$Logititude, y=taxa$Latitude, color=taxa$Genome),size=1.5)+
+  scale_size(range=c(1,1))+ 
+  scale_color_manual(values = color) +
+  theme_classic()+
   theme(axis.text.x = element_text(size = 20),axis.title.x = element_text(size = 20),axis.text.y = element_text(size = 20),axis.title.y = element_text(size = 20))+
   #scale_fill_manual(values=c("#97FFFF", "#FFD700", "#FF6347", "#8470FF","#D8BFD8"), 
  #                   breaks=c("AA", "SS", "DD", "AABB","AABBDD"),
  #                   labels=c("AA", "SS", "DD", "AABB","AABBDD"))+
   theme(axis.text = element_blank()) +   ## 删去所有刻度标签
-  theme(axis.ticks = element_blank()) +
-  theme(panel.grid =element_blank()) + 
-  theme(axis.ticks.y = element_blank())
+  theme(axis.ticks = element_blank()) 
+  #theme(panel.grid =element_blank()) + 
+  #theme(axis.ticks.y = element_blank())
 
 #AABB----
 mp2<-mp+geom_point(aes(x=taxa_AB$Longitude, y=taxa_AB$Latitude, color=taxa_AB$TreeValidatedGroupbySubspecies),size=1.5)+scale_size(range=c(1,1))+ theme_classic()+

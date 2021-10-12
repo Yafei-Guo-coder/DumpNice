c6685ffca454d1068ae65362106b5596e3b8bbb5
Yafei Guo <yafeiguo116@gmail.com>
2021-10-12 22:44:37 +0800

    Update 09_RDA.r

--- Vmap1.1R/09_RDA.r
+++ Vmap1.1R/09_RDA.r
@@ -14,18 +14,8 @@ cat header.txt Select_gene_noHeader.vcf > gene_3000.vcf
 sed '/^##/d' gene_3000.vcf | awk '{$1=null;$2=null;$3=null;$4=null;$5=null;$6=null;$7=null;$8=null;$9=null;print $0'} | sed 's/^[ \t]*//g' > all_noMiss_0.05_3000.txt
 sed 's@0/0@0@g' all_noMiss_0.05_3000.txt | sed 's@0/1@1@g' |sed 's@1/1@2@g' |sed 's@1/0@1@g' > All_noMiss_0.05_3000.txt
 
-#3. 合并vcf并随机选取3000个位点
-#vcf-concat A_Land_Select_gene.vcf.gz B_Land_Select_gene.vcf.gz D_Land_Select_gene.vcf.gz | bgzip -c > All_gene.vcf.gz
-#run_pipeline.pl -Xms10g -Xmx200g -vcf All_gene.vcf.gz -sortPositions -export All_gene.hmp.txt -exportType HapmapDiploid
-#sed '1d' All.sort.hmp.txt | shuf -n 5000 > shuf_5000.hmp.txt
-#head -n 1 All.sort.hmp.txt > shuf.header
-#cat shuf.header shuf_5000.hmp.txt > shuf_5000.hmp.txt2
-#mv shuf_5000.hmp.txt2 shuf_5000.hmp.txt
-#run_pipeline.pl -SortGenotypeFilePlugin -inputFile shuf_5000.hmp.txt -outputFile shuf_5000.sort.hmp.txt -fileType Hapmap
-#run_pipeline.pl -Xmx100g -fork1 -h shuf_5000.sort.hmp.txt -export -exportType VCF
-
-#4. 样本分区：EU, WA, SCA, EA_North, EA_SouthWest
-#5. RDA analysis----
+#3. 样本分区：EU, WA, SCA, EA_North, EA_SouthWest
+#4. RDA analysis----
 #input files:
 #env_table: data frame, row: sample name, col: environment variables
 #genotype_table: data frame, row: sample name, col: snp site
@@ -53,84 +43,12 @@ env_prec <- env_all[,13:20]
 ##tb-RDA
 #物种数据 Hellinger 预转化（处理包含很多 0 值的群落物种数据时，推荐使用）
 phylum_hel <- decostand(phylum, method = 'hellinger')
-#使用全部的环境数据
+#环境数据
 rda_tb_all <- rda(phylum_hel~., env_all, scale = FALSE)
 rda_tb_temp <- rda(phylum_hel~., env_temp, scale = FALSE)
 rda_tb_prec <- rda(phylum_hel~., env_prec, scale = FALSE)
 rda_tb_ele <- rda(phylum_hel~., env_ele, scale = FALSE)
-
-rda_tb.scaling1 <- summary(rda_tb_all, scaling = 2)
-rda_tb.scaling1
-
-#若只关注局部环境数据，除了在原始表格中修改变量个数外，还可直接在 rda() 中指定
-#rda_part <- rda(phylum~elevation+one+two+three+four+five+six+seven+eight+nine+ten+eleven+twelve+thirteen+fourteen+fifteen+sixteen+seventeen+eighteen+nineteen, data = env, scale = FALSE)
-
-#plot point-line graph---------
-
-#color = c("#838B8B", "#8470FF", "#D8BFD8", "#FF6349", "#FFD700") 
-color <- brewer.pal(n = 5, name = "Accent")[c(1:5)]
-DeleteName <-  c("ZN109","TW004","TW005","TW053","TW054","TW060","TW088","TW109","TW110","XI_29")
-
-label <- read.table("select_taxa2.txt", header=T, stringsAsFactors = F)
-label$Region <- as.factor(label$Region)
-label$cols = label$Region 
-label$cols = gsub("EA-N","#D8BFD8",label$cols)
-label$cols = gsub("EU","#838B8B",label$cols)
-label$cols = gsub("SCA","#97FFFF",label$cols)
-label$cols = gsub("WA","#FFD700",label$cols)
-label$cols = gsub("EA-S","#FF6349",label$cols)
-#label$cols = gsub("AM","#8470FF",label$cols)
-#label$cols = gsub("AF","#E7298A",label$cols)
-#rda_tb <- rda(phylum_hel~., env_all, scale = FALSE)
-#r2 <- RsquareAdj(rda_tb)
-#rda_noadj <- r2$r.squared
-#rda_adj <- r2$adj.r.squared
-#rda_tb_test <- anova(rda_tb, permutations = 999)
-#rda_tb_test_axis <- anova(rda_tb, by = 'axis', permutations = 999)
-#rda_tb_test_axis$`Pr(>F)` <- p.adjust(rda_tb_test_axis$`Pr(>F)`, method = 'bonferroni')
-#pca_eig <- rda_tb$CA$eig
-#pca_eig[pca_eig > mean(pca_eig)]
-#n <- length(pca_eig)
-#bsm <- data.frame(j=seq(1:n), p = 0)
-#bsm$p[1] <- 1/n
-#for (i in 2:n) bsm$p[i] <- bsm$p[i-1] + (1/(n + 1 - i))
-#bsm$p <- 100*bsm$p/n
-#vif.cca(rda_tb)
-#rda_tb_forward_r <- ordiR2step(rda(phylum~1, env, scale = FALSE), scope = formula(rda_tb), R2scope = rda_adj, direction = 'forward')
-#rda_tb_forward_r.scaling1 <- summary(rda_tb_forward_r, scaling = 1)
-rda_tb_forward_r.site <- data.frame(rda_tb.scaling1$sites)[1:2]
-rda_tb_forward_r.env <- data.frame(rda_tb.scaling1$biplot)[1:2]
-#group=read.table("group.txt",header = TRUE,row.names = 1,)
-rda_tb_forward_r.site$sample <- rownames(rda_tb_forward_r.site)
-#rda_tb_forward_r.site <- merge(rda_tb_forward_r.site, group, by = 'row.names')
-rda_tb_forward_r.env$sample <- rownames(rda_tb_forward_r.env)
-
-F <- rda_tb_forward_r.site[!rownames(rda_tb_forward_r.site) %in% DeleteName,]
-label$cols <- as.factor(label$cols)
-label$Region <- as.factor(label$Region)
-p <- ggplot(F, aes(RDA1, RDA2)) +
-  geom_point(aes(color = label$Region), size=3) +
-  #stat_ellipse(aes(group=label$cols), level = 0.95, show.legend = FALSE, linetype = 2) +
-  scale_color_manual(values = color) +
-  theme(panel.grid = element_blank(), panel.background = element_rect(color = 'black', fill = 'transparent'), plot.title = element_text(hjust = 0.5), legend.key = element_rect(fill = 'transparent')) + 
-  theme_classic()+
-  theme(panel.grid =element_blank()) +   ## 删去网格线
-  #theme(axis.text = element_blank()) +   ## 删去刻度标签
-  #theme(axis.ticks = element_blank()) +   ## 删去刻度线
-  theme(panel.border = element_blank()) +
-  theme(panel.grid.major = element_line(color = 'gray', size = 0.1), panel.background = element_rect(color = 'black', fill = 'transparent'))+
-  #legend.title = (element_blank(), legend.key = element_rect(fill = 'transparent'), plot.title = element_text(hjust = 0.5)) + 
-  labs(x = 'RDA1 (34.12%)', y = 'RDA2 (11.90%)') +
-  geom_vline(xintercept = 0, color = 'gray', size = 0.5) + 
-  geom_hline(yintercept = 0, color = 'gray', size = 0.5) +
-  geom_segment(data = rda_tb_forward_r.env, aes(x = 0, y = 0, xend = RDA1, yend = RDA2), arrow = arrow(length = unit(0.3, 'cm')), size = 1, color = 'brown',alpha=0.2) +
-  geom_text(data = rda_tb_forward_r.env, aes(RDA1 * 1.1, RDA2 * 1.1, label = sample), color = 'brown', size = 5)+
-  #scale_colour_discrete(breaks = c("#838B8B","#FFD700", "#97FFFF", "#D8BFD8", "#FF6349"), labels = c('EU','WA','SCA','EA-N','EA-S'))+
-  guides(fill=guide_legend(title=NULL))
-#geom_label_repel(aes(label =sample, color = group), size = 3, box.padding = unit(0, 'lines'), show.legend = FALSE)
-p
-
-#分类样本提取----
+#分类样本提取
 taxa <- read.table("select_taxa4.txt",header=T,stringsAsFactors = F,sep="\t")
 taxa_EA_N <- taxa[which(taxa$Region=="Cen_A" | taxa$Region=="NE_A" ),1]
 taxa_EA_S <- taxa[which(taxa$Region=="SW_A"| taxa$Region=="Tibet" | taxa$Region=="SA"),1]
@@ -138,12 +56,7 @@ taxa_WA <- taxa[which(taxa$Region=="WA"),1]
 taxa_SCA <- taxa[which(taxa$Region=="NW_A" | taxa$Region=="CA"),1]
 #taxa_AF <- taxa[which(taxa$Region=="AF"),1]
 taxa_EU <- taxa[which(taxa$Region=="EU"),1]
-
-#选择TAXA_old-----
-#taxa_EA_25 <- c("TW030","TW032","ZN160","TW033","TW154","TW166","TW138","ZN177","ZN001","TW153","TW158","TW167","ZN083","TW160","TW155","TW151","TW159","TW162","TW145","TW144","ZN167","ZN097","ZN166","TW134","TW147")
-#taxa_SCA_25 <- c("ZN112","ZN179","TW029","TW051","TW025","TW028","TW094","TW027","ZN111","ZN119","TW057","XI_33","TW074","TW073","TW056","TW001","TW055","TW102","ZN113","ZN118","TW113","ZN121","XI_36","ZN115","TW087")
-#taxa_EU_25 <- c("TW085","XI_10","TW070","XI_7","TW065","XI_8","TW089","XI_5","XI_6","XI_4","TW052","TW071","TW059","TW108","TW062","TW072","TW026","XI_1","TW058","TW096","TW107","TW064","TW091","TW097","TW099")
-#taxa_WA_25 <- c("ZN176","TW078","ZN175","XI_31","XI_32","TW076","TW100","TW077","TW101","TW079","TW080","TW081","TW082","TW104","TW075","TW103","TW105","ZN110","ZN174","TW002","TW003","TW106","XI_16","XI_17","TW069")
+#画不同区域的对应的不同变量的解释度barplot，重复500次----
 alltemp1 <- vector()
 allprec1 <- vector()
 allele1 <- vector()
@@ -254,21 +167,80 @@ while (x < 500){
   allele2 <- cbind(allele2,eleName2)
   x <- x+1
 }
-#Rsq----
+#Rsq
 #rownames(alltemp1) <- c("WA","EU","SCA","EA_N","EA_S")
 #rownames(allprec1) <- c("WA","EU","SCA","EA_N","EA_S")
 #Rsq <- cbind(apply(alltemp1,1,mean),apply(allprec1,1,mean),apply(alltemp1,1,sd),apply(allprec1,1,sd))
 #colnames(Rsq)<- c("temp_mean","prec_mead","temp_sd","prec_sd")
 #write.table(Rsq, "RDA_Rsq.txt", sep="\t")
-#Adjust Rsq(后续使用)----
+#Adjust Rsq(后续使用)
 rownames(alltemp2) <- c("WA","EU","SCA","EA_N","EA_S")
 rownames(allprec2) <- c("WA","EU","SCA","EA_N","EA_S")
 rownames(allele2) <- c("WA","EU","SCA","EA_N","EA_S")
 AdjRsq <- cbind(apply(alltemp2,1,mean),apply(allprec2,1,mean),apply(allele2,1,mean),apply(alltemp2,1,sd),apply(allprec2,1,sd),apply(allele2,1,sd))
 colnames(AdjRsq)<- c("temp_mean","prec_mean","ele_mean","temp_sd","prec_sd","ele_sd")
+write.table(AdjRsq, "RDA_AdjRsq_new.txt", row.names = T,sep="\t",col.names = T,quote=F)
+#修改RDA_AdjRsq_new.txt的格式，接下来画图
+
+color2 <- brewer.pal(n = 3, name = "Accent")
+AdjRsq <- read.table("RDA_AdjRsq_new.txt", header=T,sep="\t")
+AdjRsq$ID = factor(AdjRsq$ID, levels=c('EU','WA','CA_EA_N','EA_C','EA_S'))
+AdjRsq$Type = factor(AdjRsq$Type, levels=c("temp_mean","prec_mean","ele_mean"))
+ggplot(AdjRsq, aes(x=ID, y=Mean, group=Type,fill=Type)) + 
+  geom_bar(position=position_dodge(), stat="identity") +
+  geom_errorbar(aes(ymin=Mean-SE, ymax=Mean+SE),
+                width=.2, # 设置误差线的宽度 
+                position=position_dodge(.9))+
+  theme_classic()+
+  scale_fill_manual(values = color2) 
 
 
-write.table(AdjRsq, "RDA_AdjRsq.txt", row.names = T,sep="\t",col.names = T)
+#-----
+
+
+rda_tb.scaling1 <- summary(rda_tb_all, scaling = 2)
+rda_tb.scaling1
+#若只关注局部环境数据，除了在原始表格中修改变量个数外，还可直接在 rda() 中指定
+#rda_part <- rda(phylum~elevation+one+two+three+four+five+six+seven+eight+nine+ten+eleven+twelve+thirteen+fourteen+fifteen+sixteen+seventeen+eighteen+nineteen, data = env, scale = FALSE)
+#plot point-line graph---------
+
+#color = c("#838B8B", "#8470FF", "#D8BFD8", "#FF6349", "#FFD700") 
+
+label <- read.table("select_taxa4.txt", header=T, stringsAsFactors = F)
+DeleteName <-  label[is.na(label$RDA_Region),1]
+label$RDA_Region <- as.factor(label$RDA_Region)
+rda_tb_forward_r.site <- data.frame(rda_tb.scaling1$sites)[1:2]
+rda_tb_forward_r.env <- data.frame(rda_tb.scaling1$biplot)[1:2]
+#group=read.table("group.txt",header = TRUE,row.names = 1,)
+rda_tb_forward_r.site$sample <- rownames(rda_tb_forward_r.site)
+#rda_tb_forward_r.site <- merge(rda_tb_forward_r.site, group, by = 'row.names')
+rda_tb_forward_r.env$sample <- rownames(rda_tb_forward_r.env)
+
+F <- rda_tb_forward_r.site[!rownames(rda_tb_forward_r.site) %in% DeleteName,]
+F$ID <- rownames(F)
+all <- merge(F,label,by="ID")
+p <- ggplot(all, aes(RDA1, RDA2,color=RDA_Region)) +
+  geom_point( size=3) +
+  #geom_point( size=3) +
+  #stat_ellipse(aes(group=label$cols), level = 0.95, show.legend = FALSE, linetype = 2) +
+  #scale_color_manual(values = color) +
+  theme(panel.grid = element_blank(), panel.background = element_rect(color = 'black', fill = 'transparent'), plot.title = element_text(hjust = 0.5), legend.key = element_rect(fill = 'transparent')) + 
+  theme_classic()+
+  theme(panel.grid =element_blank()) +   ## 删去网格线
+  #theme(axis.text = element_blank()) +   ## 删去刻度标签
+  #theme(axis.ticks = element_blank()) +   ## 删去刻度线
+  theme(panel.border = element_blank()) +
+  theme(panel.grid.major = element_line(color = 'gray', size = 0.1), panel.background = element_rect(color = 'black', fill = 'transparent'))+
+  #legend.title = (element_blank(), legend.key = element_rect(fill = 'transparent'), plot.title = element_text(hjust = 0.5)) + 
+  labs(x = 'RDA1 (34.96%)', y = 'RDA2 (11.23%)') +
+  geom_vline(xintercept = 0, color = 'gray', size = 0.5) + 
+  geom_hline(yintercept = 0, color = 'gray', size = 0.5) +
+  geom_segment(data = rda_tb_forward_r.env, aes(x = 0, y = 0, xend = RDA1, yend = RDA2), arrow = arrow(length = unit(0.3, 'cm')), size = 1, color = 'brown',alpha=0.2) +
+  geom_text(data = rda_tb_forward_r.env, aes(RDA1 * 1.1, RDA2 * 1.1, label = sample), color = 'brown', size = 5)+
+  #scale_colour_discrete(breaks = c("#838B8B","#FFD700", "#97FFFF", "#D8BFD8", "#FF6349"), labels = c('EU','WA','SCA','EA-N','EA-S'))+
+  guides(fill=guide_legend(title=NULL))
+#geom_label_repel(aes(label =sample, color = group), size = 3, box.padding = unit(0, 'lines'), show.legend = FALSE)
+p
 
 #画各区域RDA分解图----
 plot(EU_temp_rda, type = 'n', display = c('wa', 'cn'), choices = 1:2, scaling = 1)
@@ -291,26 +263,6 @@ plot(South_west_temp_rda, type = 'n', display = c('wa', 'cn'), choices = 1:2, sc
 points(South_west_temp_rda, choices = 1:2, scaling = 1, display = 'wa', pch = 19, cex = 1)
 text(South_west_temp_rda, choices = 1:2, scaling = 1, display = 'cn', col = 'brown', cex = 1)
 
-#画整体barplot----
-#old version ----
-#version 1
-#data <- read.table("plot_data.txt", header=T, row.names = 1,stringsAsFactors = F)
-#data<-t(as.matrix(data))
-#barplot(data, beside = TRUE,
-#        col = c("lightblue", "mistyrose"),
-#        legend = rownames(data), ylim = c(0, 0.3))
 
-# version 2----
-color2 <- brewer.pal(n = 4, name = "Accent")
-AdjRsq <- read.table("RDA_AdjRsq.txt", header=T,sep="\t")
-AdjRsq$Region = factor(AdjRsq$Region, levels=c('EU','WA','SCA','EA_N','EA_S'))
-AdjRsq$Type = factor(AdjRsq$Type, levels=c("Temperature","Precipitation"))
-ggplot(AdjRsq, aes(x=Region, y=Mean, group=Type,fill=Type)) + 
-  geom_bar(position=position_dodge(), stat="identity") +
-  geom_errorbar(aes(ymin=Mean-se, ymax=Mean+se),
-                width=.2, # 设置误差线的宽度 
-                position=position_dodge(.9))+
-  theme_classic()+
-  scale_fill_manual(values = c("#FDC086","#BEAED4")) 
 
 

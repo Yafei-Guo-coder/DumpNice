18015917e54ad7b788b45453ab92f927cfadc990
Yafei Guo <yafeiguo116@gmail.com>
2021-10-16 21:49:56 +0800

    Update codes

--- 01_Vmap1.1R/08_VIP_Gene_Heatmap.r
+++ 01_Vmap1.1R/08_VIP_Gene_Heatmap.r
@@ -2,16 +2,17 @@ library(pheatmap)
 library(RColorBrewer)
 display.brewer.all()
 ##Top5_R----
-data2 <- read.table("/Users/guoyafei/Documents/01_个人项目/01_Migration/02_Add_ZNdata/02_Environment/02_XP-CLR/HeatMap/heatmap_format2.txt",header=T,
+data2 <- read.table("/Users/guoyafei/Documents/01_Migration/02_Environment/02_XP-CLR/HeatMap/V3/Top1_heatmap_format2.txt",
+                    header=T,
                     row.names= 1, stringsAsFactors=F,sep="\t")
 data <- as.matrix(data2)
 x=vector()
-for ( i in c(1:93)){
+for ( i in c(1:15)){
   x <- c(x,strsplit(colnames(data), "type_")[[i]][2])
 }
 colnames(data)<-x
 #region <- c("Strang_WA", "WA_EU", "WA_CA", "WA_NW_A", "WA_NE_A", "WA_SA", "WA_SW_A", "WA_Tibet", "WA_SE_A", "CA_SA", "SA_SW_A", "SA_Tibet", "NW_A_NE_A", "SW_A_NE_A", "SE_A_NE_A")
-region <- c("Strang_WA", "WA_EU", "WA_CA", "CA_SA", "CA_NW_A","NW_A_NE_A","SA_Tibet", "SA_SW_A", "SW_A_NE_A")
+region <- c("neg_North1_North2", "neg_WA_North1", "neg_WA_North2", "WA_EU", "Strang_WA","WA_South","Tibet_South", "North2_South")
 data <- data[region,]
 
 #非nlr基因的颜色
@@ -21,6 +22,7 @@ pheatmap(data,cluster_rows = F,cluster_cols = T,border_color=NA,color = colsA,fo
 data <- as.matrix(t(data2))
 colsA = c("#006D2C","#a3d9b8","#88419D","#e9d5f0","#FEB24C","#f7e6d0")
 pheatmap(data,cluster_rows = F,cluster_cols = F,border_color=NA,color = colsA,fontsize=8)
+
 #P值
 xlab <-factor(xlab,levels=c("Top5%","Top1%","Top0.5%","Top0.1%"))
 P <- c(106.1767,69.862,65.09623,9.649433)

68acca58bd2b9ce1341fad226932c7693318450a
Yafei Guo <yafeiguo116@gmail.com>
2021-10-16 16:17:04 +0800

    Update 01_Xp-clr_Smooth_V2.r

--- 01_Vmap1.1R/01_Xp-clr_Smooth_V2.r
+++ 01_Vmap1.1R/01_Xp-clr_Smooth_V2.r
@@ -1,8 +1,6 @@
 #XP-CLR
 #工作路径：
 #xuebo@204:/data2/xuebo/Projects/Speciation/xpclr/Selection_V3
-
-
 #XPCLR的流程：XPCLR -xpclr ../groupSouth/groupSouthChr${i}.geno ../groupSCA/groupSCAChr${i}.geno ../groupSCA/groupSCAChr${i}.snp South_SCA_10kchr${i} -w1 0.005 500 10000 $i -p1 0.95 &
 #这是参数 -w1 0.005 500 10000 -p1 0.95
 #下面的是做smooth标准化
@@ -16,7 +14,6 @@
 #XPCLRresult_neg_North1_North2
 #XPCLRresult_neg_WA_North1
 #XPCLRresult_neg_WA_North2
-
 #!/usr/bin/Rscript.R
 library(GenWin)
 library(dplyr)
@@ -43,7 +40,6 @@ for (num in c(1:9)){
   }
 }
 #Rscript smooth.R
-
 #GenWin(smooth)的结果是这样的"WindowStart" "WindowStop" "SNPcount" "MeanY" "Wstat"，没有染色体，要加上染色体的信息
 #工作路径：xuebo@204:/data2/xuebo/Projects/Speciation/xpclr/North_South_SCA/XPCLRresultSouth_SCA/smooth
 #接下来给smooth结果添加染色体号并进行排序，并且合并成A，B，D lineage.

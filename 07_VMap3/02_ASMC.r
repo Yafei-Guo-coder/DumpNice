ff1a149442c3785e2c87b785d9df1185dc2ee349
Yafei Guo <yafeiguo116@gmail.com>
2022-03-15 21:12:24 +0800

    update codes

--- 07_VMap3/02_ASMC.r
+++ 07_VMap3/02_ASMC.r
@@ -1,5 +1,5 @@
 #vcftools --gzvcf Alineage001_Wild_emmer.vcf.gz --freq --stdout | awk -F":" '{print $1"\t"$2"\t"$3}'| awk '{if($8<0.51) print $1"\t"$1"-"$2"\t"$5"\t"$7"\t"$8"\t"$4; else print $1"\t"$1"-"$2"\t"$5"\t"$7"\t"$6"\t"$4}' | sed '1c CHR\tSNP\tA1\tA2\tMAF\tNCHROBS'
-
+#66:/data1/home/yafei/004_Vmap3/ASMC
 
 for i in `cat population1.txt`
 do

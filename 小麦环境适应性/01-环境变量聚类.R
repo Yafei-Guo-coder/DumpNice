47566d9b26c1fa2383fbf5d83ec9147dfa0c4d75
Yafei Guo <yafeiguo116@gmail.com>
2023-09-14 22:05:21 +0800

    Update 01-环境变量聚类.R

--- 小麦环境适应性/01-环境变量聚类.R
+++ 小麦环境适应性/01-环境变量聚类.R
@@ -222,7 +222,6 @@ ggplot(PC, aes(x=type,y=PC1,color=region,fill=region)) +
   theme_classic()
 
 ###############################################环境距离和遗传距离的相关性############################################
-
 #方式一：(在计算环境距离/地理距离和遗传距离的相关性时使用），根据环境PC选变量的问题：环境PC最相关的变量，在基因组上相应的位点很少，并不能很好的反应环境适应性。除了prec是PC2，其余都是PC1.
 #  temp11(0.98);temp1(0.96);temp6(0.96)(temp1鉴定的又多又好)
 #  solar3(0.99);solar1(0.94)(solar1鉴定的又多又好)
@@ -230,21 +229,15 @@ ggplot(PC, aes(x=type,y=PC1,color=region,fill=region)) +
 #    更新成prec3 prec4 prec6上面prec1;prec5鉴定的位点都很少
 #  soil9(0.94);soil12(0.94);soil13(0.93);soil11(-0.83);soil10(soil12鉴定的又多又好)
 #    更新成soil12(0.94)上面soil9，soil13，soil11鉴定位点情况都不好,位点也比较少
-#方式二：（计算群体结构，定义最终适应性位点的时候使用）根据基因组贡献程度以及环境之间的相关性(小于0.3)，选择环境变量(V4)
-#  temp8;temp9;temp4,temp5,temp2,elevation,temp7,temp3
-#  solar1;solar2
-#  prec7;prec8;prec4,prec3,prec6（剔除了prec8）
-#  soil8;soil7;soil10;soil9;soil6,soil12;soil3(因为生物学意义，去掉soil6;soil8;soil7)
 
 #环境变量的筛选
 library(ggplot2)
+library(tidyr)
+library(reshape2)
+library (geosphere)
 setwd("/Users/guoyafei/Desktop/3type/fst")
 data <- read.table("471_baypass_taxa.Info", header=T,row.names = 1,stringsAsFactors = F)
 env <- data[,c(4:3,6:40)]
-
-library(tidyr)
-library(reshape2)
-library ("geosphere")
 
 #地理位置距离
 geo_sub <- data[,c("Longitude","Latitude","type")]
@@ -274,6 +267,7 @@ all <- c(1,4,6,7,10,13,15,16,17,18,26,34,35,8,9,11,12,27,28,29,33,3,30,31,32,36,
 
 #now
 #prec <- c(4,6,7,10,13,15,16,17,18,26,34,35,38)
+#取和环境变量PC1最相关的变量
 solar <- c(1,24)
 temp <- c(1,28)
 prec <- c(1,14)
@@ -296,104 +290,6 @@ for ( i in c(1:4)) {
   #df3 <- geo_sub 
   #colnames(df3) <- colnames(geo_sub)
   result3 <- as.data.frame(as.matrix(dist(env_sub, method = "euclidean")))
-  row.names(result3) <- paste("pop",1:25,sep="")
-  colnames(result3) <- paste("pop",1:25,sep="")
-  result3$id <- rownames(result3)
-  c<- melt(result3,id="id")
-  c$seq <- paste(c$id,c$variable,sep="-")
-  env_order <- c[order(c$seq),]
-  
-  #遗传距离
-  #PC1
-  data <- read.table(file[i], header=T,stringsAsFactors = F)
-  data2 <- data[,c(1,3,2,4,5)]
-  colnames(data2) <- colnames(data)
-  #meanfst
-  all <- rbind(data,data2)[,c(2,3,4)]
-  duijiao <- read.table("对角线.txt",header=T,stringsAsFactors = F)
-  all <- rbind(all,duijiao)
-  all[which(all$value < 0),3] <- 0
-  all$seq <- paste(all$id1,all$id2,sep="-")
-  fst_order <- all[order(all$seq),]
-  out <- cbind(geo_order,fst_order)
-  colnames(out) <- c("id1","id2","geo","seq1","id3","id4","fst","seq2")
-  out$type <- name[i]
-  a <- cor.test(out$geo,out$fst)
-  
-  out2 <- cbind(env_order,fst_order)
-  colnames(out2) <- c("id1","id2","env","seq1","id3","id4","fst","seq2")
-  out2$type <- name[i]
-  a2 <- cor.test(out2$env,out2$fst)
-  
-  #遗传距离
-  #随机变异
-  data <- read.table("shuf_fst.txt", header=T,stringsAsFactors = F)
-  data2 <- data[,c(1,3,2,4,5)]
-  colnames(data2) <- colnames(data)
-  #meanfst
-  all <- rbind(data,data2)[,c(2,3,4)]
-  duijiao <- read.table("对角线.txt",header=T,stringsAsFactors = F)
-  all <- rbind(all,duijiao)
-  all[which(all$value < 0),3] <- 0
-  all$seq <- paste(all$id1,all$id2,sep="-")
-  shuf_order <- all[order(all$seq),]
-  shuf_out <- cbind(geo_order,shuf_order)
-  colnames(shuf_out) <- c("id1","id2","geo","seq1","id3","id4","fst","seq2")
-  shuf_out$type <- "shuf"
-  b <- cor.test(shuf_out$geo,shuf_out$fst)
-  
-  shuf_out2 <- cbind(env_order,shuf_order)
-  colnames(shuf_out2) <- c("id1","id2","env","seq1","id3","id4","fst","seq2")
-  shuf_out2$type <- "shuf"
-  b2 <- cor.test(shuf_out2$env,shuf_out2$fst)
-  
-  #plot:geo_fst
-  pdf(file_out[i],height=3,width=4)
-  til <- paste(a$p.value,as.numeric(a$estimate),b$p.value,as.numeric(b$estimate),sep=",")
-  p <- rbind(out,shuf_out)
-  pp <- ggplot(p, aes(x=geo/100000, y=fst/(1-fst),color=type)) +
-    geom_point(size=0.5,alpha=0.3)+
-    theme_bw()+
-    xlab("Geographic distance(100km)")+
-    ggtitle(til)+
-    geom_smooth(method = "lm", se=TRUE, 
-                formula = y ~ x) +
-    scale_color_manual(values = c("#E69F00","#56B4E9"))
-  
-  #plot:env_fst
-  q <- rbind(out2,shuf_out2)
-  til2 <- paste(a2$p.value,as.numeric(a2$estimate),b2$p.value,as.numeric(b2$estimate),sep=",")
-  qq <- ggplot(q, aes(x=env, y=fst/(1-fst),color=type)) +
-    geom_point(size=0.5,alpha=0.3)+
-    theme_bw()+
-    xlab("Environmental distance")+
-    ggtitle(til2)+
-    geom_smooth(method = "lm", se=TRUE, 
-                formula = y ~ x) +
-    scale_color_manual(values = c("#E69F00","#56B4E9"))
-  print(pp)
-  print(qq)
-  dev.off()
-}
-
-#方式二：用环境PC1计算环境的距离(这种方法效果不好)
-setwd("/Users/guoyafei/Desktop/GF/等位基因频率变化")
-PC1 <- read.table("/Users/guoyafei/Desktop/baypass/群体变量.txt",header=T,stringsAsFactors = F)
-PC <- PC1[,c(1,4,5,2,3)]
-
-name <- c("all","prec","soil","solar","temp")
-file <- paste(name,"-R-V4_fst.txt",sep="")
-file_out <- paste(name,"-all-R-PC2.pdf",sep="")
-#所有环境做PC,结果存储在*-all-R-PC*.pdf
-PC_file <- paste(name,"-PC.txt",sep="")
-#只有筛选过（有一定的遗传贡献以及R<0.3)的环境变量做PC）效果稍微好一点，结果存储在*-sub-R-PC*.pdf
-#PC_file <- paste(name,"-sub-PC.txt",sep="")
-for ( i in c(2:5)) {
-  #环境距离
-  #env2 <- PC[,i,drop=F]
-  env <- read.table(PC_file[i],header=T,stringsAsFactors = F)
-  env2 <- env[,2,drop=F]
-  result3 <- as.data.frame(as.matrix(dist(env2, method = "euclidean")))
   row.names(result3) <- paste("pop",1:25,sep="")
   colnames(result3) <- paste("pop",1:25,sep="")
   result3$id <- rownames(result3)
@@ -523,10 +419,196 @@ for (i in 1){
   dev.off()
 }
 
-#########################################################环境变量的三角图###################################################
+##################################################环境变量的三角图###################################################
 #首先对环境变量取PC1的值
+library(ggplot2)
+library(tidyr)
+library(reshape2)
+library (geosphere)
+setwd("/Users/guoyafei/Desktop/3type/三角图")
+data <- read.table("/Users/guoyafei/Desktop/3type/fst/471_baypass_taxa.Info", header=T,row.names = 1,stringsAsFactors = F)
+env <- data[,c(4:3,6:40)]
+sub <- data[,c(3,6:40)]
+#sub2 <- sub[,c(1:9,11:14,22:36)]
+#sub3 <-  sub2[!duplicated(sub2, fromLast=TRUE), ] 
+type1_var <- c("solar1","solar2","solar3","prec3","prec4","prec6","prec7","temp2","temp8")
+type2_var <- c("temp1","temp5","temp6","temp9","temp10","temp11","Elevation")
+type3_var <- c("prec1","prec2","prec5","prec8","temp3","temp4","temp7","soil9","soil10","soil11","soil12","soil13")
+
+#环境变量PCA
+dt = scale(sub, center = T, scale = T)
+var <- list(type1_var <- type1_var, type2_var <- type2_var, type3_var <- type3_var)
+a <- rep(paste("pop",1:25,sep=""), 25)
+b <- paste("pop",rep(1:25,each=25),sep="")
+c<-paste(a,b,sep="-")
+all <- c[order(c)]
+for(i in c(1:3)){
+  type <- dt[,which(colnames(dt) %in% var[[i]])]
+  #type2 <- dt[,which(colnames(dt) %in% type2_var)]
+  #type3 <- dt[,which(colnames(dt) %in% type3_var)]
+  rm1 <- cor(type)
+  rs1<- eigen(rm1)
+  #提取结果中的特征值，即各主成分的方差；
+  val <- rs1$values
+  #计算方差贡献率和累积贡献率；
+  Proportion_of_Variance <- val/sum(val)
+  #提取主成分的方差贡献率，生成坐标轴标题；
+  pc1<-paste0("PC1(",round(Proportion_of_Variance[1]*100,2),"%)")
+  pc2<-paste0("PC2(",round(Proportion_of_Variance[2]*100,2),"%)")
+  pc3<-paste0("PC3(",round(Proportion_of_Variance[3]*100,2),"%)")
+  #提取结果中的特征向量(也称为Loadings,载荷矩阵)；
+  U<-as.matrix(rs1$vectors)
+  #进行矩阵乘法，获得PC score；
+  PC <- type %*% U
+  colnames(PC) <- paste("PC",1:dim(PC)[2],sep="")
+  PC <- as.data.frame(PC)
+  PC$type <- data$type
+  M <- aggregate(PC, by=list(PC$type),FUN=mean)
+  env_sub <- M[,c(2,3),drop=F]
+  result3 <- as.data.frame(as.matrix(dist(env_sub, method = "euclidean")))
+  row.names(result3) <- paste("pop",1:25,sep="")
+  colnames(result3) <- paste("pop",1:25,sep="")
+  result3$id <- rownames(result3)
+  c<- melt(result3,id="id")
+  c$seq <- paste(c$id,c$variable,sep="-")
+  type_order <- c[order(c$seq),]
+  all <- cbind(all,as.numeric(type_order[,3]))
+}
+all_dist <- as.data.frame(all)
+colnames(all_dist) <- c("pop","type1","type2","type3")
+all_dist$pop <- as.character(all_dist$pop)
+all_dist$type1 <- as.numeric(all_dist$type1)
+all_dist$type2 <- as.numeric(all_dist$type2)
+all_dist$type3 <- as.numeric(all_dist$type3)
+sub_dist <- all_dist[which(all_dist$type1 != 0 & all_dist$type2 != 0 & all_dist$type3 != 0),]
+
+#画图测试
+ggplot(data = M,aes(x=PC1,y=PC2,color=as.factor(type)))+
+  geom_point()+
+  labs(x=pc1,y=pc2,color="")+
+  guides(fill=F)+
+  theme_classic()
 
 
+
+
+#地理位置距离
+geo_sub <- data[,c("Longitude","Latitude","type")]
+M <- aggregate(geo_sub, by=list(geo_sub$type),FUN=mean)
+row.names(M) <- paste("pop",M$type,sep="")
+geo_sub <- M[,colnames(M)[2:3],drop=F]
+result3 <- as.data.frame(as.matrix(distm(geo_sub, fun = distGeo)))
+row.names(result3) <- paste("pop",1:25,sep="")
+colnames(result3) <- paste("pop",1:25,sep="")
+result3$id <- rownames(result3)
+c<- melt(result3,id="id")
+c$seq <- paste(c$id,c$variable,sep="-")
+geo_order <- c[order(c$seq),]
+
+
+
+#方式一：用多环境合并计算环境的距离，取和环境变量PC1最相关的变量
+solar <- c(1,24)
+temp <- c(1,28)
+prec <- c(1,14)
+
+seq <- list(all,prec,solar,temp)
+
+name <- c("all","prec","solar","temp")
+#file <- paste(name,c("PC1V3_fst.txt","PC1V2_fst.txt","PC1_fst.txt","PC1_fst.txt"),sep="")
+#file <- c("allPC1V3_fst.txt","precPC1V2_fst.txt","type1-R-V5_fst.txt","type2-R-V5_fst.txt")
+
+file <- c("allPC1V3_fst.txt","type3-R-V5_fst.txt","type1-R-V5_fst.txt","type2-R-V5_fst.txt")
+file_out <- paste(name,".pdf",sep="")
+for ( i in c(1:4)) {
+  #环境距离
+  env2 <- env[,seq[[i]]]
+  M <- aggregate(env2, by=list(env2$type),FUN=mean)
+  row.names(M) <- paste("pop",M$type,sep="")
+  env_sub <- M[,colnames(env2)[-1],drop=F]
+  #df3 = scale(geo_sub,center = T,scale = T)
+  #df3 <- geo_sub 
+  #colnames(df3) <- colnames(geo_sub)
+  result3 <- as.data.frame(as.matrix(dist(env_sub, method = "euclidean")))
+  row.names(result3) <- paste("pop",1:25,sep="")
+  colnames(result3) <- paste("pop",1:25,sep="")
+  result3$id <- rownames(result3)
+  c<- melt(result3,id="id")
+  c$seq <- paste(c$id,c$variable,sep="-")
+  env_order <- c[order(c$seq),]
+  
+  #遗传距离
+  #PC1
+  data <- read.table(file[i], header=T,stringsAsFactors = F)
+  data2 <- data[,c(1,3,2,4,5)]
+  colnames(data2) <- colnames(data)
+  #meanfst
+  all <- rbind(data,data2)[,c(2,3,4)]
+  duijiao <- read.table("对角线.txt",header=T,stringsAsFactors = F)
+  all <- rbind(all,duijiao)
+  all[which(all$value < 0),3] <- 0
+  all$seq <- paste(all$id1,all$id2,sep="-")
+  fst_order <- all[order(all$seq),]
+  out <- cbind(geo_order,fst_order)
+  colnames(out) <- c("id1","id2","geo","seq1","id3","id4","fst","seq2")
+  out$type <- name[i]
+  a <- cor.test(out$geo,out$fst)
+  
+  out2 <- cbind(env_order,fst_order)
+  colnames(out2) <- c("id1","id2","env","seq1","id3","id4","fst","seq2")
+  out2$type <- name[i]
+  a2 <- cor.test(out2$env,out2$fst)
+  
+  #遗传距离
+  #随机变异
+  data <- read.table("shuf_fst.txt", header=T,stringsAsFactors = F)
+  data2 <- data[,c(1,3,2,4,5)]
+  colnames(data2) <- colnames(data)
+  #meanfst
+  all <- rbind(data,data2)[,c(2,3,4)]
+  duijiao <- read.table("对角线.txt",header=T,stringsAsFactors = F)
+  all <- rbind(all,duijiao)
+  all[which(all$value < 0),3] <- 0
+  all$seq <- paste(all$id1,all$id2,sep="-")
+  shuf_order <- all[order(all$seq),]
+  shuf_out <- cbind(geo_order,shuf_order)
+  colnames(shuf_out) <- c("id1","id2","geo","seq1","id3","id4","fst","seq2")
+  shuf_out$type <- "shuf"
+  b <- cor.test(shuf_out$geo,shuf_out$fst)
+  
+  shuf_out2 <- cbind(env_order,shuf_order)
+  colnames(shuf_out2) <- c("id1","id2","env","seq1","id3","id4","fst","seq2")
+  shuf_out2$type <- "shuf"
+  b2 <- cor.test(shuf_out2$env,shuf_out2$fst)
+  
+  #plot:geo_fst
+  pdf(file_out[i],height=3,width=4)
+  til <- paste(a$p.value,as.numeric(a$estimate),b$p.value,as.numeric(b$estimate),sep=",")
+  p <- rbind(out,shuf_out)
+  pp <- ggplot(p, aes(x=geo/100000, y=fst/(1-fst),color=type)) +
+    geom_point(size=0.5,alpha=0.3)+
+    theme_bw()+
+    xlab("Geographic distance(100km)")+
+    ggtitle(til)+
+    geom_smooth(method = "lm", se=TRUE, 
+                formula = y ~ x) +
+    scale_color_manual(values = c("#E69F00","#56B4E9"))
+  
+  #plot:env_fst
+  q <- rbind(out2,shuf_out2)
+  til2 <- paste(a2$p.value,as.numeric(a2$estimate),b2$p.value,as.numeric(b2$estimate),sep=",")
+  qq <- ggplot(q, aes(x=env, y=fst/(1-fst),color=type)) +
+    geom_point(size=0.5,alpha=0.3)+
+    theme_bw()+
+    xlab("Environmental distance")+
+    ggtitle(til2)+
+    geom_smooth(method = "lm", se=TRUE, 
+                formula = y ~ x) +
+    scale_color_manual(values = c("#E69F00","#56B4E9"))
+  print(pp)
+  print(qq)
+  dev.off()
+}
 
 
 
